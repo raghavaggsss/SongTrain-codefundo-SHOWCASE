1f47a2e77a87720b031f19099eafcebe4d76756c
raghavaggsss <zacefron.efron727@gmail.com>
2017-05-07 00:43:42 +0100

    song shows up immediately after upload

--- sendEmail.py
+++ sendEmail.py
@@ -12,6 +12,6 @@ def send_email(mail_id,song_name):
 	smtpserver.login(gmail_user, gmail_pwd)
 
 	header = 'To:' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject:Your Karaoke is ready! Please upload recording. \n'
-	msg = header + '\n We have generated the karaoke for your audio file named - %s. Please login using the link (https://songtraincfd.southindia.cloudapp.azure.com/Project/) to view your profile and proceed. \n\n'%song_name
+	msg = header + '\n We have generated the karaoke for your audio file named - %s. Please login using the link ( https://songtraincfd.southindia.cloudapp.azure.com/Project/ ) to view your profile and proceed. \n\n'%song_name
 	smtpserver.sendmail(gmail_user, to, msg)
 	smtpserver.close()
